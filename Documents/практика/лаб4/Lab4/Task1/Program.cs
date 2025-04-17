1b31e3d5af658fc4086115fc9050f02bd4369e4d
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-17 13:20:12 +0300

    Initial commit

---
+++ Documents/практика/лаб4/Lab4/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class FunctionTable
+{
+    public static double CalculateFunction(double x, double a, double b)
+    {
+        if (x >= 0.9)
+            return 1 / Math.Pow(b + x, 2);
+        else if (x >= 0 && x < 0.9)
+            return a * x + 0.1;
+        else
+            return Math.Pow(x, 2) + b;
+    }
+
+    public static void BuildTable(double a, double b, double h)
+    {
+        Console.WriteLine("Таблица значений функции:");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("|   x   |    f(x)    |");
+        Console.WriteLine("----------------------------");
+
+        for (double x = a; x <= b; x += h)
+        {
+            double y = CalculateFunction(x, a, b);
+            Console.WriteLine($"| {x:F2} | {y:F6} |");
+        }
+
+        Console.WriteLine("----------------------------");
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите значение a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите шаг h:");
+        double h = Convert.ToDouble(Console.ReadLine());
+
+        BuildTable(a, b, h);
+    }
+}
---
+++ Documents/практика/лаб4/Lab4/Task3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Matrix
+{
+    private int[,] data; 
+    public int Rows { get; }
+    public int Cols { get; }
+
+    public Matrix(int rows, int cols)
+    {
+        Rows = rows;
+        Cols = cols;
+        data = new int[rows, cols];
+
+        Random rand = new Random();
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                data[i, j] = rand.Next(1, 11);
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+                Console.Write(data[i, j] + "\t");
+            Console.WriteLine();
+        }
+    }
+
+    public static Matrix operator +(Matrix m1, Matrix m2)
+    {
+        if (m1.Rows != m2.Rows || m1.Cols != m2.Cols)
+            throw new InvalidOperationException("Матрицы должны быть одинакового размера!");
+
+        Matrix result = new Matrix(m1.Rows, m1.Cols);
+
+        for (int i = 0; i < m1.Rows; i++)
+            for (int j = 0; j < m1.Cols; j++)
+                result.data[i, j] = m1.data[i, j] + m2.data[i, j];
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите размер матриц:");
+        Console.Write("Количество строк: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Количество столбцов: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+
+        Matrix matrix1 = new Matrix(rows, cols);
+        Matrix matrix2 = new Matrix(rows, cols);
+
+        Console.WriteLine("\nПервая матрица:");
+        matrix1.Print();
+
+        Console.WriteLine("\nВторая матрица:");
+        matrix2.Print();
+
+        try
+        {
+            Matrix resultMatrix = matrix1 + matrix2;
+            Console.WriteLine("\nРезультат сложения матриц:");
+            resultMatrix.Print();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
